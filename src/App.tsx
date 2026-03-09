f6c4e6f8c8ccc0099083eae7abe3d60398d86fd4
Amanda_charaz <alimacharamanda@gmail.com>
2026-03-09 12:05:47 +0200

    fix App.tsx routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Dashboard from "./pages/Dashboard"
-import Members from "./pages/Members"
-import Visitors from "./pages/Visitors"
-import Finance from "./pages/Finance"
-import Announcements from "./pages/Announcements"
-
-import ProtectedRoute from "./components/ProtectedRoute"
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Members from './pages/Members'
+import Visitors from './pages/Visitors'
+import Finance from './pages/Finance'
+import Announcements from './pages/Announcements'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        {/* PUBLIC PAGES */}
-        <Route path="/" element={<Announcements />} />
-        <Route path="/announcements" element={<Announcements />} />
-
-        {/* LOGIN */}
-        <Route path="/login" element={<Login />} />
-
-        {/* PROTECTED DASHBOARD */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/members"
-          element={
-            <ProtectedRoute>
-              <Members />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/visitors"
-          element={
-            <ProtectedRoute>
-              <Visitors />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/finance"
-          element={
-            <ProtectedRoute>
-              <Finance />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/members" element={<ProtectedRoute><Members /></ProtectedRoute>} />
+          <Route path="/visitors" element={<ProtectedRoute><Visitors /></ProtectedRoute>} />
+          <Route path="/announcements" element={<ProtectedRoute><Announcements /></ProtectedRoute>} />
+          <Route path="/finance" element={<ProtectedRoute><Finance /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
